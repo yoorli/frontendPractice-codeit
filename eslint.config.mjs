66ddd5ccb701ccd3b49bae79171d954285972f66
yoolri <lucca6042@gmail.com>
2025-11-11 19:55:30 +0900

    chore: install libs and configure linting
    - add .eslintrc.* and eslint.config.*
    - add .prettierrc and .prettierignore
    - update package.json scripts (lint, format)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +14,16 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "warn",
+      "simple-import-sort/exports": "warn",
+    },
+  },
 ]);
 
 export default eslintConfig;
