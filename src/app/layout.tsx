55397f3912fbb0a536a07923f87750b9f2fa1c7b
yoolri <lucca6042@gmail.com>
2025-11-20 13:49:09 +0900

    feat: upload eleventh practice component
    test Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import Link from 'next/link';
@@ -35,6 +36,7 @@ export default function RootLayout({
           <Header />
         </Link>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
